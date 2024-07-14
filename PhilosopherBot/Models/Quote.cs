bf9cc2add3752fd307b4e1546ff258bfe53c5a0d
Yauheni Kuntsevich <jauhien.k@gmail.com>
2024-07-14 23:32:00 +0300

    Quote model was added by Comments properties

--- PhilosopherBot/Models/Quote.cs
+++ PhilosopherBot/Models/Quote.cs
@@ -5,6 +5,8 @@ public class Quote
     public int Id { get; set; }
     public string Author { get; set; } = null!;
     public string Text { get; set; } = null!;
+    public string? CommentAboutText { get; set; }
     public string? ImagePath { get; set; }
+    public string? CommentAboutImage { get; set; }
     public string? StickerUrl { get; set; }
 }
